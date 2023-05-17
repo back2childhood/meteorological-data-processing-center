d1aa5bea14a82e4cc5f1488d9aec9b739c3adf33
back2childhood <1939371557@qq.com>
2023-05-18 00:21:20 +0800

    first commit

---
+++ public/_cmpublic.h
@@ -0,0 +1,54 @@
+/****************************************************************************************
+ *   程序名：_cmpublic.h，此程序是开发框架公用头文件，包含了应用开发常用的头文件。
+ *   作者：Jili Qian
+ *****************************************************************************************/
+
+#ifndef _cmpublic_H
+#define _cmpublic_H
+
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <locale.h>
+#include <math.h>
+#include <netdb.h>
+#include <poll.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <time.h>
+#include <unistd.h>
+#include <utime.h>
+// #include <sys/epoll.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+// #include <sys/timerfd.h>
+
+#include <algorithm>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <string>
+#include <vector>
+
+// 采用stl标准库的命名空间std
+using namespace std;
+
+#endif
